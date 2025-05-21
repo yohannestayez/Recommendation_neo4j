b8f0a5df8804d1d7383656d490da5de99fe824a4
Yohannes Taye <ytaye715@gmail.com>
2025-05-21 15:10:05 +0300

    minor update

--- utils/db_connector.py
+++ utils/db_connector.py
@@ -6,20 +6,24 @@ import os
 load_dotenv('.env')
 
 # Set environment variables
-NEO4J_URI =  os.getenv("NEO4J_URL")
+NEO4J_URL = os.getenv("NEO4J_URL")
 NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
 NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
 
-
 class Neo4jConnector:
-    def __init__(self, uri, username, password):
-        self.driver = GraphDatabase.driver(uri, auth=(username, password))
+    def __init__(self, url, username, password):
+        try:
+            self.driver = GraphDatabase.driver(url, auth=(username, password))
+        except Exception as e:
+            raise Exception(f"Failed to connect to Neo4j: {str(e)}")
 
-    def query(self, cypher_query):
+    def query(self, cypher_query, **kwargs):
         with self.driver.session() as session:
-            session.run(cypher_query)
+            result = session.run(cypher_query, **kwargs)  # Pass parameters as kwargs
+            return [record for record in result]  # Return list of records
 
     def close(self):
         self.driver.close()
 
-DB_CONNECTOR=Neo4jConnector(url=NEO4J_URI,username=NEO4J_USERNAME, password=NEO4J_PASSWORD)
+# Initialize the connector
+DB_CONNECTOR = Neo4jConnector(url=NEO4J_URL, username=NEO4J_USERNAME, password=NEO4J_PASSWORD)
